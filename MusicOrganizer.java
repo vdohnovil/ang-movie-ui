50d52b22f65f3890571865cff4cef1b43b0f558a
vdohnovil <122384983+vdohnovil@users.noreply.github.com>
2020-12-28 06:23:09 +0000

    Init Project

---
+++ MusicOrganizer.java
@@ -0,0 +1,16 @@
+
+import java.util.ArrayList;
+import java.util.*;
+
+/**
+ * A class to hold details of audio tracks.
+ * Individual tracks may be played.
+ * 
+ * @author David J. Barnes and Michael Kölling
+ * @version 2016.02.29
+ */
+public class MusicOrganizer
+{
+    // An ArrayList for storing music tracks.
+    private ArrayList<Track> tracks;
+    // A player for the music tracks.

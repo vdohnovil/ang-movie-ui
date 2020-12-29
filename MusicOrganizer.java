d9b0dc0abddf38edea5fe302a8756944c2a830fc
vdohnovil <122384983+vdohnovil@users.noreply.github.com>
2020-12-29 21:23:21 +0000

    Updated POM

--- MusicOrganizer.java
+++ MusicOrganizer.java
@@ -14,3 +14,20 @@ public class MusicOrganizer
     // An ArrayList for storing music tracks.
     private ArrayList<Track> tracks;
     // A player for the music tracks.
+    private MusicPlayer player;
+    // A reader that can read music files and load them as tracks.
+    private TrackReader reader;
+
+    /**
+     * Create a MusicOrganizer
+     */
+    public MusicOrganizer()
+    {
+        tracks = new ArrayList<>();
+        player = new MusicPlayer();
+        reader = new TrackReader();
+        readLibrary("../audio");
+        System.out.println("Music library loaded. " + getNumberOfTracks() + " tracks.");
+        System.out.println();
+    }
+    

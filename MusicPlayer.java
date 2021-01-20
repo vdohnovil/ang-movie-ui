6975a10efd921c7b88de30f0cb91aeed0bf8b1e5
vdohnovil <122384983+vdohnovil@users.noreply.github.com>
2021-01-20 22:41:27 +0000

    hello world

--- MusicPlayer.java
+++ MusicPlayer.java
@@ -1,2 +1,16 @@
 
 import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.IOException;
+import javazoom.jl.decoder.JavaLayerException;
+import javazoom.jl.player.AudioDevice;
+import javazoom.jl.player.FactoryRegistry;
+import javazoom.jl.player.advanced.AdvancedPlayer;
+
+/**
+ * Provide basic playing of MP3 files via the javazoom library.
+ * See http://www.javazoom.net/
+ * 
+ * @author David J. Barnes and Michael Kölling.
+ * @version 2016.02.29

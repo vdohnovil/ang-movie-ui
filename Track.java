86449785f8f1369bbdf50a9d8f0fbfd2aea74a3b
vdohnovil <122384983+vdohnovil@users.noreply.github.com>
2021-05-31 08:02:29 +0000

    pom.xml

--- Track.java
+++ Track.java
@@ -20,4 +20,4 @@ public class Track
      * @param title The track's title.
      * @param filename The track file. 
      */
-    public Track(String artist, S
+    public Track(String artist, String title, String 

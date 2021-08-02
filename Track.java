e78cbda63322eb8f2bc182e361a127474f7d9c5b
vdohnovil <122384983+vdohnovil@users.noreply.github.com>
2021-08-02 15:05:43 +0000

    Update time.lua

--- Track.java
+++ Track.java
@@ -65,4 +65,6 @@ public class Track
         
     /**
      * Return details of the track: artist, title and file name.
-     * @return The track's 
+     * @return The track's details.
+     */
+   

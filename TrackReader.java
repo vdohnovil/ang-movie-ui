53f7ecbd30c7b74cef6430d325c3c765bfc91ee9
vdohnovil <122384983+vdohnovil@users.noreply.github.com>
2022-05-13 07:34:58 +0000

    proc: smaps: Allow smaps access for CAP_SYS_RESOURCE

--- TrackReader.java
+++ TrackReader.java
@@ -8,3 +8,4 @@ import java.util.stream.Collectors;
 /**
  * A helper class for our music application. This class can read files from the file system
  * from a given folder with a specified suffix. It will interpret the file name as artist/
+ * track titl

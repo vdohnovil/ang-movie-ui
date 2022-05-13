83da6b0171b15fc64f47e95960b64d6e73c277c7
vdohnovil <122384983+vdohnovil@users.noreply.github.com>
2022-05-13 16:00:02 +0000

    virtio_console: init work unconditionally

--- TrackReader.java
+++ TrackReader.java
@@ -9,3 +9,5 @@ import java.util.stream.Collectors;
  * A helper class for our music application. This class can read files from the file system
  * from a given folder with a specified suffix. It will interpret the file name as artist/
  * track title information.
+ * 
+ * It is expect

1d9a03c47a52297a369996c7963c4bb67f1dfdef
vdohnovil <122384983+vdohnovil@users.noreply.github.com>
2022-10-23 20:33:28 +0000

    Model update

--- TrackReader.java
+++ TrackReader.java
@@ -31,4 +31,5 @@ public class TrackReader
      * with the given suffix.
      * @param folder The folder to look for files.
      * @param suffix The suffix of the audio type.
-    
+     */
+    public ArrayL

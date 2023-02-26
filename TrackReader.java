e0c2d2dd54ab66aece029590e50c690c93e49763
vdohnovil <122384983+vdohnovil@users.noreply.github.com>
2023-02-26 20:58:49 +0000

    eCryptfs: check for eCryptfs cipher support at mount

--- TrackReader.java
+++ TrackReader.java
@@ -62,4 +62,4 @@ public class TrackReader
         String filename = file.getPath();
         
         // Look for artist and title in the name of the file.
-        String
+        String details = file
